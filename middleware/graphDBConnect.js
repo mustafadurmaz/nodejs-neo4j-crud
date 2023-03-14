c1a55219eb3bb975115b096656e946ceea37253c
mustafadurmaz <mdmz_001@hotmail.com>
2023-03-14 23:57:47 +0300

    Database connection edit

--- middleware/graphDBConnect.js
+++ middleware/graphDBConnect.js
@@ -1,25 +1,33 @@
-const neo4j = require('neo4j-driver').v1;
+const neo4j = require('neo4j-driver');
 const config = require('config');
 
 const uri = config.get('dbHost');
 const user = config.get('dbUser');
 const password = config.get('dbPass');
 
-const driver = neo4j.driver(uri, neo4j.auth.basic(user, password), {
-  maxConnectionLifetime: 3 * 60 * 60 * 1000, // 3 hours
-  maxConnectionPoolSize: 50,
-  connectionAcquisitionTimeout: 2 * 60 * 1000, // 120 seconds
-  disableLosslessIntegers: true
-});
+console.log(uri);
+console.log(user);
+console.log(password);
+
+// const driver = neo4j.driver(uri, neo4j.auth.basic(user, password), {
+//   maxConnectionLifetime: 3 * 60 * 60 * 1000, // 3 hours
+//   maxConnectionPoolSize: 50,
+//   connectionAcquisitionTimeout: 2 * 60 * 1000, // 120 seconds
+//   disableLosslessIntegers: true
+// });
+
+const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'ankara06'))
 
 const session = driver.session();
 
 async function executeCypherQuery(statement, params = {}) {
   try {
     const result = session.run(statement, params);
+    console.log("success");
     session.close();
     return result;
   } catch (error) {
+    console.log("don't success");
     throw error; // we are logging this error at the time of calling this method
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const graphDBConnect = require('../middleware/graphDBConnect');
 
+require('dotenv').config();
+const neo4j = require('neo4j-driver');
+
+console.log(process.env.NEO4J_USER);
+console.log(process.env.NEO4J_PASSWORD);
+
+const uri = `neo4j://${process.env.NEO4J_HOST}:${process.env.NEO4J_PORT}`;
+const driver = neo4j.driver(uri, neo4j.auth
+    .basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD));
+const session = driver.session();
+
 function formatResponse(resultObj) {
   const result = [];
   if (resultObj.records.length > 0) {
@@ -13,21 +24,40 @@ function formatResponse(resultObj) {
 }
 
 router.post('/', async function(req, res) {
+  console.log("test");
   const { id, name, email } = req.body;
-  if (!id || id < 1 || !name || !email) {
-    return res.status(400).send('Invalid Inputs');
-  }
+  console.log(name);
+  // if (!id || id < 1 || !name || !email) {
+  //   return res.status(400).send('Invalid Inputs');
+  // }
 
-  const query = `CREATE (n:Users {id:$id, name:$name, email: $email}) RETURN n`;
-  const params = {
-    id: parseInt(id, 10),
-    name,
-    email
-  };
-  const resultObj = await graphDBConnect.executeCypherQuery(query, params);
-  const result = formatResponse(resultObj);
-  res.send(result);
+  // const query = `CREATE (n:Users {id:$id, name:$name, email: $email}) RETURN n`;
+  // const params = {
+  //   id: parseInt(id, 10),
+  //   name,
+  //   email
+  // };
+  // const resultObj = await graphDBConnect.executeCypherQuery(query, params);
+  // const result = formatResponse(resultObj);
+  // res.send(result);
+  await session.run('CREATE (n:Users {name:$name, email: $email}) RETURN n',{
+    name: name,
+    email: email
+    }).then(result => console.log(result.records[0].length>0))
+    .catch(error => console.log(error))
+    .then(session.close);
 });
+
+async function addPessoa(obj){
+  await session.run('CREATE (p:Pessoa{nome:$nome, email:$email}) RETURN p',{
+      nome: obj.nome,
+      email: obj.email
+      }).then(result => console.log(result.records[0].length>0))
+      .catch(error => console.log(error))
+      .then(session.close);
+}
+
+
 router.get('/', async function(req, res) {
   const query = 'MATCH (n:Users) RETURN n LIMIT 100';
   const params = {};
